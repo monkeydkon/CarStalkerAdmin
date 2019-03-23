6ceaddc6aca76699b285b39d6122cd9360ff5d4c
MonkeyDkon <kon_fu2@hotmail.com>
2019-03-23 20:06:45 +0200

    finds timestamps

--- app/src/main/java/com/example/carstalkeradmin/UsersActivity.java
+++ app/src/main/java/com/example/carstalkeradmin/UsersActivity.java
@@ -1,10 +1,13 @@
 package com.example.carstalkeradmin;
 
+import android.app.Dialog;
 import android.icu.util.ULocale;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -43,6 +46,7 @@ public class UsersActivity extends AppCompatActivity {
 
         final ArrayList<String> arrayList = new ArrayList<>();
 
+
         mDatabase.child("users").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -72,9 +76,58 @@ public class UsersActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
                 user = arrayList.get(position);
+                setContentView(R.layout.activity_users_chosen);
+
+                final ProgressBar secondSpinner = findViewById(R.id.chosenProgressBar);
+
+                secondSpinner.setVisibility(View.VISIBLE);
+
+                final ListView listViewChosen = findViewById(R.id.chosenListView);
+
                 Toast.makeText(getApplicationContext(),user,Toast.LENGTH_SHORT).show();
+
+                final ArrayList<String> chosenArrayList = new ArrayList<>();
+
+                mDatabase.child("users").child(user).child("events").addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.exists()){
+                            for(DataSnapshot i : dataSnapshot.getChildren()){
+                                if(!chosenArrayList.contains(i.getKey())){
+                                    chosenArrayList.add(i.getKey());
+                                }
+                            }
+
+
+                        }else{
+                            Toast.makeText(getApplicationContext(),"no entries",Toast.LENGTH_SHORT).show();
+                        }
+                        ArrayAdapter secondArrayAdapter = new ArrayAdapter(getApplicationContext(),android.R.layout.simple_list_item_1,chosenArrayList);
+                        listViewChosen.setAdapter(secondArrayAdapter);
+
+                        secondSpinner.setVisibility(View.GONE);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+                listViewChosen.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Toast.makeText(getApplicationContext(),"yoyoyo",Toast.LENGTH_SHORT).show();
+                        // TODO: 23/3/2019  
+                    }
+                });
+
             }
         });
+
+
     }
+
 }
